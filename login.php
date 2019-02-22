bb3be475323829e0d53f6a1c473b9f58362e8eb9
Yeshi <tentibet59@gmail.com>
2019-02-22 16:21:37 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,69 @@
+<?php include('server.php') ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>SB Admin - Login</title>
+
+    <!-- Custom fonts for this template-->
+    <link
+      href="vendor/fontawesome-free/css/all.min.css"
+      rel="stylesheet"
+      type="text/css"
+    />
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin.css" rel="stylesheet" />
+  </head>
+
+  <body class="bg-dark">
+    <div class="container">
+      <div class="card card-login mx-auto mt-5">
+        <div class="card-header">Login</div>
+        <div class="card-body">
+          <form method="post" action="login.php">
+            <!-- <?php include('errors.php'); ?> -->
+            <div class="form-group">
+              <label for="exampleInputEmail1">Username</label>
+              <input class="form-control" type="text" name="username" />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input class="form-control" type="password" name="password" />
+            </div>
+            <div class="form-group">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" type="checkbox" /> Remember
+                  Password</label
+                >
+              </div>
+            </div>
+            <button
+              type="submit"
+              class="btn btn-primary btn-block"
+              name="login_user"
+            >
+              Login
+            </button>
+          </form>
+          <div class="text-center">
+            <a class="d-block small mt-3" href="register.php"
+              >Register an Account</a
+            >
+            <!-- <a class="d-block small" href="forgot-password.php">Forgot Password?</a>-->
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
